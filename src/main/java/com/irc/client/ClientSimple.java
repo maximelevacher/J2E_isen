407bfec154f8be92e1e8bd45c719e0335b37beac
Zakaria YAHI <zakyahi@msn.com>
2016-12-06 23:31:52 +0100

    Ajoute les tests pour le ClientSimple Enlève le main du ClientSimple et fix qqes problemes dans le projet

--- src/main/java/com/irc/client/ClientSimple.java
+++ src/main/java/com/irc/client/ClientSimple.java
@@ -5,19 +5,17 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.Socket;
-import java.net.UnknownHostException;
-import java.util.Scanner;
 
 import org.apache.log4j.Logger;
-import org.apache.log4j.PropertyConfigurator;
 
 /**
  * Cette classe implémente un client pouvant se connecter sur un serveur de chat
  * @author Zak
  *
  */
-public class ClientSimple implements Runnable {
+public class ClientSimple {
 	/**
 	 * Permet de logger des messages suivant le fichier de configuration log4j.properties
 	 */
@@ -25,11 +23,11 @@ public class ClientSimple implements Runnable {
 	static final String logConfigPath = "conf/log4j.properties";
 
 	public final static int DEFAULT_PORT = 45612;
-
+	private final static int CONNECT_TIMEOUT = 1000;
+	
 	private Socket _socket = null;
 	private PrintWriter _out = null;
 	private BufferedReader _in = null;
-	private volatile boolean _isRunning = true;
 
 	/**
 	 * Connecte le client au serveur indiqué
@@ -38,7 +36,8 @@ public class ClientSimple implements Runnable {
 	 * @throws IOException En cas de problème de connexion
 	 */
 	public void connectToServer(InetAddress hote, int port) throws IOException {
-		_socket = new Socket(hote, port);
+		_socket = new Socket();
+		_socket.connect(new InetSocketAddress(hote, port), CONNECT_TIMEOUT);
 		_out = new PrintWriter(_socket.getOutputStream(), true);
 		_in = new BufferedReader(new InputStreamReader(_socket.getInputStream()));
 		logger.info("Connecté au serveur: " + hote.toString() + ":" + port);
@@ -63,12 +62,15 @@ public class ClientSimple implements Runnable {
 	
 	/**
 	 * Attends un message du serveur. La méthode est bloquante.
-	 * @return Un string contenant le message se terminant pas un \r ou \n
+	 * @return Un string contenant le message se terminant par un \r ou \n
 	 * @throws IOException
 	 */
 	public String receiveMessage() throws IOException {
 		String message = null;
 		message = _in.readLine();
+		if(message == null) {
+			throw new IOException("Impossible de récupérer un message.");
+		}
 		return message;
 	}
 	
@@ -76,7 +78,6 @@ public class ClientSimple implements Runnable {
 	 * Ferme les canaux de communication et se deconnecte du serveur.
 	 */
 	public void disconnectFromServer() {
-		_isRunning = false;
 		_out.close();
 		try {
 			_in.close();
@@ -85,72 +86,5 @@ public class ClientSimple implements Runnable {
 		} catch (IOException e) {
 			logger.error("Erreur lors de la déconnexion.", e);
 		}
-	}
-
-	public boolean isRunning() {
-		return _isRunning;
-	}
-
-	@Override
-	public void run() {
-		logger.info("Lancement du thread receiveMessage.");
-		while(_isRunning) {
-			String message = null;
-			try {
-				message = receiveMessage();
-				if (message == null) {
-					throw new IOException("Probleme lors de la réception d'un message non reçu.");
-				}
-				logger.info("Message reçu: " + message);
-			} catch (IOException e) {
-				logger.error("Thread receiveMessage ne reçoit pas.", e);
-				disconnectFromServer();
-			}
-		}
-	}
-	
-	public static void main(String[] args) {
-		PropertyConfigurator.configure(logConfigPath);
-		InetAddress hote = null;
-		int port = ClientSimple.DEFAULT_PORT;
-		// Récupère les informations de connexion depuis les arguments du programme
-		try {
-			if (args.length >= 1) {
-				hote = InetAddress.getByName(args[0]);
-			} else {
-				hote = InetAddress.getLocalHost();
-			}
-
-			if (args.length == 2) {
-				port = Integer.parseInt(args[1]);
-			}
-		} catch (UnknownHostException e) {
-			logger.error("Erreur argument hote.", e);
-		}
-		ClientSimple client = new ClientSimple();
-		try {
-			client.connectToServer(hote, port);
-		} catch (IOException e) {
-			logger.error("Impossible de se connecter au serveur:" + hote.toString() + ":" + port, e);
-			throw new RuntimeException();
-		}
-		Thread threadClient = new Thread(client);
-		threadClient.start();
-		
-		Scanner scanner = new Scanner(System.in);
-
-		System.out.println("Entrez votre pseudo:");
-		client.setNickName(scanner.nextLine());
-
-		while(client.isRunning()) {
-			String input = scanner.nextLine();
-			if (input.equals("/quit")) {
-				logger.info("Commande entrée: " + "/quit");
-				client.disconnectFromServer();
-				break;
-			}
-			client.sendMessage(input);
-		}
-		client.disconnectFromServer();
 	}
 }
--- src/test/java/com/irc/client/ClientSimpleTest.java
+++ src/test/java/com/irc/client/ClientSimpleTest.java
@@ -2,6 +2,16 @@ package com.irc.client;
 
 import static org.junit.Assert.*;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Vector;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -9,9 +19,61 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class ClientSimpleTest {
+	
+	ServerSocket ss = null;
+	Vector<ServerSideClient> connectedClients = null;
+	Thread thread = null;
+	volatile boolean isRunning = true;
+	
+	static Vector<String> messageTestReceiveSequence = new Vector<String>();
+	
+	class ServerSideClient implements Runnable {
+		Socket _s;
+		PrintWriter _out;
+		BufferedReader _in;
+		Vector<String> messageRecus = new Vector<String>();
+		
+		ServerSideClient(Socket s) {
+			_s = s;
+			try {
+				_in = new BufferedReader(new InputStreamReader(_s.getInputStream()));
+				_out = new PrintWriter(_s.getOutputStream(), true);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
+
+		@Override
+		public void run() {
+			while(true) {
+				try {
+					String message = _in.readLine();
+					messageRecus.addElement(message);
+					if (message.startsWith("%nickname")) {
+						String nickname = message.split(" ")[1];
+						_out.println("Nickname:" + nickname);
+					} else if (message.equals("Start Test Receive Sequence")) {
+						for (String s : messageTestReceiveSequence) {
+							_out.println(s);
+						}
+					} else if (message.equals("Disconnect Server")) {
+						_s.close();
+					}
+				} catch (IOException e) {
+					//e.printStackTrace();
+				}
+			}
+		}
+	}
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		messageTestReceiveSequence.add("Test");
+		messageTestReceiveSequence.add("");
+		messageTestReceiveSequence.add("ЁЂЃЄЅІЇЈЉЊЋЌЍЎЏАБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдежзийклмнопрстуфхцчшщъыьэюя");
+		messageTestReceiveSequence.add("찦차를 타고 온 펲시맨과 쑛다리 똠방각하");
+		messageTestReceiveSequence.add("Stop Test");
 	}
 
 	@AfterClass
@@ -20,15 +82,158 @@ public class ClientSimpleTest {
 
 	@Before
 	public void setUp() throws Exception {
+		ss = new ServerSocket();
+		ss.setReuseAddress(true);
+		ss.bind(new InetSocketAddress(55555));
+		
+		connectedClients = new Vector<ServerSideClient>();
+		isRunning = true;
+		
+		thread = new Thread() {
+			public void run() {
+				while(isRunning) {
+					try {
+						ServerSideClient sc = new ServerSideClient(ss.accept());
+						Thread tsc= new Thread(sc);
+						tsc.start();
+						connectedClients.addElement(sc);
+					} catch (IOException e) {
+						//e.printStackTrace();
+					}
+				}
+			}
+		};
+		thread.start();
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		isRunning = false;
+
+		try {
+			ss.close();
+			thread.join();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Test
-	public void testMain() {
-		//fail("Not yet implemented");
+	public void testConnexionReussie() {
+		ClientSimple client = new ClientSimple();
+		try {
+			client.connectToServer(InetAddress.getLocalHost(), 55555);
+			client.sendMessage("Test");
+		} catch (IOException e) {
+			e.printStackTrace();
+			fail("La connexion n'a pas réussie.");
+		}
 	}
+	
+	@Test
+	public void testConnexionRatee() {
+		ClientSimple client = new ClientSimple();
+		try {
+			client.connectToServer(InetAddress.getLocalHost(), 55554);
+			client.sendMessage("Test");
+			fail("Le client a réussi à se connecter.");
+		} catch (IOException e) {
+		}
+		
+		client = new ClientSimple();
+		try {
+			client.connectToServer(InetAddress.getByName("8.8.8.8"), 55555);
+			client.sendMessage("Test");
+			fail("Le client a réussi à se connecter.");
+		} catch (IOException e) {
+		}
+	}
+	
+	@Test
+	public void testDisconnect() {
+		ClientSimple client = new ClientSimple();
+		try {
+			client.connectToServer(InetAddress.getLocalHost(), 55555);
+		} catch (IOException e) {
+		}
+		client.disconnectFromServer();
+		try {
+			client.receiveMessage();
+			fail("Le client est resté connecté.");
+		} catch (Exception e) {
+		}
+	}
+	
+	@Test
+	public void testEnvoiMessage() {
+		ClientSimple client = new ClientSimple();
+		Vector<String> listOfMessageToSend = new Vector<String>();
+		
+		listOfMessageToSend.add("Test");
+		listOfMessageToSend.add("");
+		listOfMessageToSend.add("X5O!P%@AP[4\\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*");
+		listOfMessageToSend.add("!@#$%^&*()`~");
+		listOfMessageToSend.add("ЁЂЃЄЅІЇЈЉЊЋЌЍЎЏАБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдежзийклмнопрстуфхцчшщъыьэюя");
+		listOfMessageToSend.add("찦차를 타고 온 펲시맨과 쑛다리 똠방각하");
 
+		try {
+			client.connectToServer(InetAddress.getLocalHost(), 55555);
+			for (String s : listOfMessageToSend) {
+				client.sendMessage(s);
+			}
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			assertEquals(listOfMessageToSend, connectedClients.get(0).messageRecus);
+		} catch (IOException e) {
+		}
+	}
+	
+	@Test
+	public void testReceiveMessage() {
+		ClientSimple client = new ClientSimple();
+		Vector<String> messageRecus = new Vector<String>();
+		try {
+			client.connectToServer(InetAddress.getLocalHost(), 55555);
+			client.sendMessage("Start Test Receive Sequence");
+			String message = null;
+			do {
+				message = client.receiveMessage();
+				messageRecus.add(message);
+			} while (!message.equals("Stop Test"));
+			assertEquals(messageTestReceiveSequence, messageRecus);
+		} catch (IOException e) {
+		}
+	}
+	
+	@Test
+	public void testReceiveMessageServerDisconnected() {
+		ClientSimple client = new ClientSimple();
+		try {
+			client.connectToServer(InetAddress.getLocalHost(), 55555);
+			client.sendMessage("Disconnect Server");
+		} catch (IOException e) {
+		}
+		try {
+			client.receiveMessage();
+			fail("Le message a bien été reçu du serveur.");
+		} catch (IOException e) {
+			assertEquals("Impossible de récupérer un message.", e.getMessage());
+		}
+	}
+	
+	@Test
+	public void testSetNickName() {
+		ClientSimple client = new ClientSimple();
+		
+		try {
+			client.connectToServer(InetAddress.getLocalHost(), 55555);
+			client.setNickName("Zak");
+			String message = client.receiveMessage();
+			assertEquals("Nickname:Zak", message);
+		} catch (IOException e) {
+		}
+	}
 }
