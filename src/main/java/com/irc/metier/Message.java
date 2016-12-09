8f6ccc3ba8e1ec1cb999880f4791e9d7b6706a6c
Neptune <Neptune@192.168.1.28>
2016-12-09 22:57:28 +0100

    Ajout des entités Metier Personne et Message

---
+++ src/main/java/com/irc/metier/Message.java
@@ -0,0 +1,64 @@
+package com.irc.metier;
+
+import java.sql.Date;
+
+public class Message {
+	private int num;
+	private String message;
+	private Personne sender;
+	private Personne emmetter;
+	private Date date;
+	
+	public Message(int num,String message,Personne sender, Personne emmetter){
+		this.num = num;
+		this.message = message;
+		this.sender=sender;
+		this.emmetter = emmetter;
+		java.util.Date date_util = new java.util.Date();
+		this.date =new java.sql.Date(date_util.getTime());
+	}
+
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public Personne getSender() {
+		return sender;
+	}
+
+	public void setSender(Personne sender) {
+		this.sender = sender;
+	}
+
+	public Personne getEmmetter() {
+		return emmetter;
+	}
+
+	public void setEmmetter(Personne emmetter) {
+		this.emmetter = emmetter;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	public String toString(){
+		return " "+sender.getNickname()+" > "+getMessage();
+	}
+}
---
+++ src/main/java/com/irc/metier/Personne.java
@@ -0,0 +1,29 @@
+package com.irc.metier;
+
+public class Personne {
+	private int id;
+	private String nickname;
+	
+	public Personne(int id,String nickname){
+		this.id = id;
+		this.nickname=nickname;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	
+}
+
