8b4b7dccf7f3e55e56ccc80e4ff025682890aad2
Zakaria YAHI <zakyahi@msn.com>
2016-12-15 22:39:04 +0100

    Corrige le test d'over connexions sur le serveur

--- src/test/java/com/irc/server/ServerMultiClientTest.java
+++ src/test/java/com/irc/server/ServerMultiClientTest.java
@@ -171,7 +171,7 @@ public class ServerMultiClientTest {
 					out.writeObject("%ping");
 					// Et on tente de lire une réponse du serveur.
 					// Si elle renvoie null, le serveur a fermé la connexion
-					Thread.sleep(1000);
+					Thread.sleep(1500);
 					if(in.readObject() == null) {
 						throw new Exception("Client n'a pas pu se connecter");
 					}
